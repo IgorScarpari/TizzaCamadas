ad35c1e531e2c5c3f84904ef95f0ec5dbfd5578c
IgorScarpari <igorscarpari2002@gmail.com>
2024-03-27 21:50:26 -0300

    Incluído Pizzaria

---
+++ Servicos/ServPizzaria.cs
@@ -0,0 +1,31 @@
+﻿using Entidades;
+using Repositorio;
+
+namespace Servicos
+{
+    public interface IServPizzaria
+    {
+        void Inserir(InserirPizzariaDTO inserirPizzariaDTO);
+    }
+
+    public class ServPizzaria
+    {
+        private IRepoPizzaria _repoPizzaria;
+        
+        public ServPizzaria(IRepoPizzaria repoPizzaria)
+        {
+            _repoPizzaria = repoPizzaria;
+        }
+
+        public void Inserir(InserirPizzariaDTO inserirPizzariaDTO)
+        {
+            var pizzaria = new Pizzaria();
+
+            pizzaria.Nome = inserirPizzariaDTO.Nome;
+            pizzaria.Telefone = inserirPizzariaDTO.Telefone;
+            pizzaria.Endereco = inserirPizzariaDTO.Endereco;
+
+            _repoPizzaria.Inserir(pizzaria);
+        }
+    }
+}
